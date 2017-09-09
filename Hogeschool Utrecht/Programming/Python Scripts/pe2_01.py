87980b3a8bf7309b8a887872aa2562a36f802476
dylanrozendom <dylan.rozendom@student.hu.nl>
2017-09-09 14:36:00 +0200

    Done

---
+++ Hogeschool Utrecht/Programming/Python Scripts/pe2_01.py
@@ -0,0 +1,15 @@
+letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
+A = letters.count('A')
+B = letters.count('B')
+C = letters.count('C')
+print(A, B, C)
+
+cijferICOR = 7
+cijferPROG = 6
+cijferCSN = 7
+cijfers = [cijferICOR, cijferPROG, cijferCSN]
+average = sum(cijfers)
+print (average / 2)
+
+beloningpunten = 30
+
